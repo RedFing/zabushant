cac7e1d05469fec4f5e656428b01379be08ff02a
RedFing <muhamed.ahmatovic@gmail.com>
2018-01-02 13:38:48 +0100

    simple get route

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,19 @@ var models  = require('../models');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/:id', function (req,res,next) {
+    console.log(req.params);
+    const  {id}  = req.params;
+    models.User.findById(id)
+        .then(user => {
+            const rUser = user.get({ plain: true });
+            res.send(rUser);
+        })
+        .catch(err => {
+            res.status(404);
+            res.send("User not found");
+        });
+});
 router.post('/', function(req,res,next){
   const { username, email, password } = req.body;
   models.User.create({ username, email, password })
