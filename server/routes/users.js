b8aeaaf017235188d512da4aeacf4ef30ad9c2f7
RedFing <muhamed.ahmatovic@gmail.com>
2018-01-02 13:09:19 +0100

    added simple post route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var models  = require('../models');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post('/', function(req,res,next){
+  const { username, email, password } = req.body;
+  models.User.create({ username, email, password })
+      .then((user) => {
+        console.log('user created with id: ', user.get('id'));
+        res.send({ status: 'OK'});
+      })
+      .catch(err => res.send({ status: 'ERROR'}));
+});
 module.exports = router;
