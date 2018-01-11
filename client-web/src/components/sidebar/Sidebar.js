fb8100dd8be90a665bccd9257b33d6b8039eb1a2
ahmatovic <muhamed.ahmatovic@gmail.com>
2018-01-11 03:07:52 +0100

    sidebar and main component refactor

--- client-web/src/components/sidebar/Sidebar.js
+++ client-web/src/components/sidebar/Sidebar.js
@@ -3,7 +3,7 @@ import {  Menu, Grid, Form, Icon, Dropdown } from 'semantic-ui-react';
 import './Sidebar.css';
 import Channels from './Channels';
 import DirectMessages from './DirectMessages';
-import Chat from '../chat/Chat';
+
 
 class Sidebar extends Component {
     constructor(props){
@@ -11,24 +11,26 @@ class Sidebar extends Component {
         this.state={
             activeChannel: null,
             channelSearch: ''
-        }
+        };
     }
     render() {
         const trigger = <Icon name='list layout'/>;
+        // TODO: options never change in render, extract them
         const options = [
             { key: 'user', text: 'Account', icon: 'user' },
             { key: 'reset-password', text: 'Reset password', icon: 'lock',  as: 'a' },
             { key: 'settings', text: 'Settings', icon: 'settings' },
             { key: 'sign-out', text: 'Sign Out', icon: 'sign out' },
         ];
-        const { channels, username } = this.props;
-        const directMessages = channels.filter(c => c.isDirectMessage).filter(c => c.name.includes(this.state.channelSearch));
-        const groupChannels = channels.filter(c => !c.isDirectMessage).filter(c => c.name.includes(this.state.channelSearch));
+        const { channels, username, channelName } = this.props;
+        const { channelSearch } = this.state;
+        const directMessages = channels.filter(c => c.isDirectMessage).filter(c => c.name.includes(channelSearch));
+        const groupChannels  = channels.filter(c => !c.isDirectMessage).filter(c => c.name.includes(channelSearch));
         return (
                 <Grid.Column style={{width:'250px'}}>
                     <Menu vertical>
                         <Menu.Item>
-                            {this.props.username}
+                            {username}
                             <Dropdown trigger={trigger} options={options} pointing='top left' icon={null} />
                         </Menu.Item>
                         <Menu.Item className='search-custom'>
@@ -37,12 +39,24 @@ class Sidebar extends Component {
                                 placeholder="search channels...."
                                 fluid
                                 size="mini"
-                                value={this.state.channelSearch}
+                                value={channelSearch}
                                 onChange={(e) => this.setState({ channelSearch: e.target.value })}
                             />
                         </Menu.Item>
-                        <Channels channelName={this.props.channelName} onChannelChange={this.props.onChannelChange} channels={groupChannels}/>
-                        <DirectMessages channelName={this.props.channelName} username={this.props.username} onChannelChange={this.props.onChannelChange} channels={directMessages}/>
+                        {/* TODO: these two components are nearly identical, use one instead but make
+                            it render differently based on additional props passed
+                        */}
+                        <Channels
+                          channelName={channelName}
+                          onChannelChange={this.props.onChannelChange}
+                          channels={groupChannels}
+                        />
+                        <DirectMessages
+                          channelName={channelName}
+                          username={this.props.username}
+                          onChannelChange={this.props.onChannelChange}
+                          channels={directMessages}
+                        />
                     </Menu>
                 </Grid.Column>
         );
--- client-web/src/components/zabushant/Zabushant.js
+++ client-web/src/components/zabushant/Zabushant.js
@@ -5,7 +5,17 @@ import Chat from '../chat/Chat';
 import socketClient from 'socket.io-client';
 import axios from 'axios';
 import Loader from '../../components/loader/Loader';
-import { ToastContainer, toast} from 'react-toastify';
+
+
+// TODO extract
+const api = {
+  getCurrentUser          : ()          => axios.get('/get-current-user'),
+  getAllChannels          : (userId)    => axios.get(`/get-all-channels/${userId}`),
+  getAllMessagesForChannel: (channelId) => axios.get(`/get-all-messages/${channelId}`),
+};
+
+// TODO: extract constant
+const SOCKET_ENDPOINT = "http://127.0.0.1:5000";
 
 class Zabushant extends Component {
     constructor(props){
@@ -15,22 +25,21 @@ class Zabushant extends Component {
             messages: [],
             channels: [],
             user: {},
-            endpoint: "http://127.0.0.1:5000",
             loading: true,
             socket: {},
-
         }
     }
     onChannelChange = (channelId) => {
         this.setState({ currentChannel: channelId});
-
     };
+
     getChannelName = () => {
         const { currentChannel, channels} = this.state;
         const selectedChannel = channels.find(c => c.ChannelId === currentChannel);
         if (selectedChannel && selectedChannel.name) return selectedChannel.name;
         return '';
     };
+
     sendMessage = (content) => {
         const messageObj = {
             userId: this.state.user.id,
@@ -38,73 +47,81 @@ class Zabushant extends Component {
             channelId: this.state.currentChannel,
             username: this.state.user.username
         };
-        this.state.socket.emit('message', messageObj);
+        this.socket.emit('message', messageObj);
     };
+
+    onMessageReceived = (newMessage) => {
+        const { messages } = this.state;
+        let findChannelId = messages.findIndex(c => c.channelId === newMessage.channelId);{}
+        if (findChannelId>-1){
+            let channelToInsert = messages[findChannelId];
+            channelToInsert.channelMessages = [...channelToInsert.channelMessages,
+              { username: newMessage.username, content:newMessage.content, createdAt: newMessage.createdAt}];
+            let messagesCopy = this.state.messages;
+            messagesCopy[findChannelId] = channelToInsert;
+            this.setState({ messages: messagesCopy});
+        }
+    };
+
+    initSocket = () => {
+      const socket = socketClient(SOCKET_ENDPOINT);
+      this.socket = socket;
+      socket.on('connect', () => socket.emit('Register username', this.state.user));
+      socket.on('received message', (newMessage) => this.onMessageReceived(newMessage));
+    };
+
+    // TODO: add error handling
+    async componentDidMount(){
+        let currentUser = await api.getCurrentUser();
+        this.setState({ user: currentUser.data}, this.initSocket);
+        let allChannels = await api.getAllChannels(currentUser.data.id);
+        this.setState({ channels: allChannels.data});
+        let allMessages = await axios.all(allChannels.data.map(c => api.getAllMessagesForChannel(c.ChannelId)));
+        const allMessages2 = allMessages.map((ch,i) =>
+          ({channelId: allChannels.data[i].ChannelId, channelMessages: ch.data}));
+        this.setState({ currentChannel: allChannels.data[0].ChannelId, messages: allMessages2, loading: false});
+    }
+    /*
     componentDidMount() {
         axios.get('/get-current-user')
             .then( res => {
-               this.setState({ user: res.data }, () => {
-                   const { endpoint } = this.state;
-                   const socket = socketClient(endpoint);
-                    this.setState({ socket });
-                   socket.on('connect', () => {
-                       socket.emit('Register username', this.state.user);
-                   });
-                   socket.on('received message', (newMessage) =>{
-                       console.log('from socket', newMessage);
-                       let findChannel = this.state.messages.findIndex(c => c.channelId === newMessage.channelId);{}
-                       if (findChannel>-1){
-                           let channelToInsert = this.state.messages[findChannel];
-                           channelToInsert.channelMessages = [...channelToInsert.channelMessages,
-                               { username: newMessage.username, content:newMessage.content, createdAt: newMessage.createdAt}];
-                           let messagesCopy = this.state.messages;
-                           messagesCopy[findChannel] = channelToInsert;
-                           this.setState({ messages: messagesCopy});
-                       }
-                   } )
-               });
-               axios.get(`/get-all-channels/${this.state.user.id}`)
-                   .then(res => {
-                       this.setState({ channels: res.data});
-                       console.log('resDATA IS', res.data);
-                       const allChannelMessagesRequests = res.data.map(c => {
-                           return  axios.get(`/get-all-messages/${c.ChannelId}`)
-                       });
-
-                       axios.all(allChannelMessagesRequests)
-                           .then(allRes => {
-                               const all = allRes.map((res2,i) => {
-                                   return {
-                                       channelId: res.data[i].ChannelId,
-                                       channelMessages: res2.data
-                                   }
-                               });
-                               this.setState({ currentChannel: res.data[0].ChannelId, messages: all, loading: false});
-
-                           })
-                           .catch(err => this.setState({ err: true}));
-
-                   })
-            });
-    }
+                this.setState({ user: res.data }, this.initSocket);
+                return axios.get(`/get-all-channels/${this.state.user.id}`)
+            })
+            .then(res => {
+              this.setState({channels: res.data});
+              const allChannelMessagesRequests = res.data.map(c => {
+                return axios.get(`/get-all-messages/${c.ChannelId}`)
+              });
+              return axios.all(allChannelMessagesRequests)
+            })
+            .then(allRes => {
+               const { channels } = this.state;
+               const all = allRes.map((res2,i) =>
+                 ({channelId: channels[i].ChannelId, channelMessages: res2.data}));
+               this.setState({ currentChannel: channels[0].ChannelId, messages: all, loading: false});
+            })
+            .catch(err => this.setState({ err: true}));
+    }*/
 
     render() {
-        const { messages, currentChannel, loading } = this.state;
-        if (loading) return <Loader/>
+        const { messages, currentChannel, channels, user, loading } = this.state;
+        if (loading) return <Loader/>;
         return (
             <div>
-            <Grid>
-                <Sidebar
-                    username={this.state.user.username}
-                    onChannelChange={this.onChannelChange}
-                    channels={this.state.channels}
-                    channelName={this.getChannelName()}
-                />
-                <Chat
-                    sendMessage={this.sendMessage}
-                    channelName={this.getChannelName()}
-                    messages={messages.find(msgObj => msgObj.channelId === currentChannel)} />
-            </Grid>
+                <Grid>
+                    <Sidebar
+                        username={user.username}
+                        onChannelChange={this.onChannelChange}
+                        channels={channels}
+                        channelName={this.getChannelName()}
+                    />
+                    <Chat
+                        sendMessage={this.sendMessage}
+                        channelName={this.getChannelName()}
+                        // TODO: this prop calculation is not optimized
+                        messages={messages.find(msgObj => msgObj.channelId === currentChannel)} />
+                </Grid>
             </div>
         );
     }
